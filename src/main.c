9259ce56c59958d6f84b2064ced4dce58259f723
gitmario3d <140595777+gitmario3d@users.noreply.github.com>
2024-09-19 12:47:56 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -12,10 +12,10 @@
 
 #include "../inc/philo.h"
 
-void	ft_check_args(t_info *info)
+void	ft_check_args(char **argv)
 {
-	if (info->philo_numbers <= 0 | info->philo_numbers > 200)
-		error_exit("invalid number of philosophers");
+	if (argv[1] <= 0 | argv[1] > 200)
+		error_exit("invalid number of philosophers", NULL);
 }
 
 int	main(int argc, char **argv)
@@ -23,8 +23,8 @@ int	main(int argc, char **argv)
 	t_info	*info;
 
 	if (argc > 5 | argc < 4)
-		error_exit("invalid number of arguments");
+		ft_error_exit("invalid number of arguments", NULL);
+	ft_check_args(argv);
 	ft_init_info(info, argv);
-	ft_check_args(info);
 	return (0);
 }
